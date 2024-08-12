934a7e81831d6ed6377e582e3eae046c20f1b9c9
priest-2105 <fawzybailey782@gmail.com>
2024-08-12 18:25:29 +0100

    commit ✨✨

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,22 @@
+let age = 25;
+let year = 2019
+
+console.log(age, year);
+
+age = 30
+console.log(age);
+
+
+const points = 100; 
+console.log(points);
+
+
+var score = 75;
+console.log(score);
+
+
+
+ 
+let undefinesample = null;
+
+console.log(undefinesample);
