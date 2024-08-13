a9cc30a6275e51c68febebc00c21a2f06af1d6b0
priest-2105 <fawzybailey782@gmail.com>
2024-08-13 13:00:51 +0100

    chapter ✨✨

---
+++ chapter_3/sandbox.js
@@ -0,0 +1,39 @@
+let radius = 10
+const pi = 3.14
+
+
+// console.log(radius, pi);
+
+
+// matth operators +, -, *,, / , **, %
+
+
+// console.log(10/2);
+
+// let result = radius % 3;
+// let result = pi * radius**2
+
+
+
+// order of operation  - B I D M A S
+
+// let result = 5 * (10 - 3)**2;
+
+// console.log(result);
+
+
+
+let likes = 10;
+
+// result = likes + 10;
+
+// likes++;
+// likes--;
+
+// result =likes += 10
+// like  -= 5;
+// likes *= 10;
+// likes /= 10;
+
+
+console.log(likes);
---
+++ chapter_4/sandbox.js
@@ -0,0 +1,19 @@
+// common string methodss 
+
+
+
+let email = 'mario@thenetninja.co.uk';
+
+// let result = email.lastIndexOf('n');
+
+// let result = email.slice(4,14)
+
+// let result = email.substr(4,14)
+
+// let result = email.replace('o', '9')
+
+// let result = email.replace('o', '9')
+
+console.log(result);
+
+
