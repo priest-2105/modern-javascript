c2b35e512c65533bbcf7f03af4897af344386a12
priest-2105 <fawzybailey782@gmail.com>
2024-08-13 22:48:31 +0100

    commit ✨✨✨

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,1 +1,26 @@
+// Booleans & Comparisons
+
 console.log(true, false, "true", "false");
+
+
+
+// methods can return booleans
+let email = 'luigi@thenetninja.co.uk';
+let names = ['mario', 'luigi', 'toad'];
+
+// let result = email.includes('@');
+// let result = names.includes('luigi');
+
+// console.log(result);
+
+
+
+// comparison operators 
+
+let age = 25;
+
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 25);
+console.log(age > 25);
+
