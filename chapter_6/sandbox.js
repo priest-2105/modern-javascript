dec63062082827e938bda971d86d75622333175f
priest-2105 <fawzybailey782@gmail.com>
2024-08-13 22:53:41 +0100

    commit ✨✨✨

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -19,8 +19,21 @@ let names = ['mario', 'luigi', 'toad'];
 
 let age = 25;
 
-console.log(age == 25);
-console.log(age == 30);
-console.log(age != 25);
-console.log(age > 25);
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 25);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+
+let name = 'shaun';
+
+
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
 
