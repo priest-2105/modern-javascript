a306847ae4ac3684757d0dc3bf29d79e0b27010a
priest-2105 <fawzybailey782@gmail.com>
2024-08-14 10:29:21 +0100

    update ✨✨

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,16 +1,16 @@
 let ninjas = ['shaun', 'ryu', 'chun-li']
 
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
+ninjas[1] = 'ken';
+console.log(ninjas[1]);
 
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
+// l/et ages = [20, 25, 30, 35];
+console.log(ages[2]);
 
 
 let random = [ 'shaun', 'crystal', 30, 20 ];
-// console.log(random);
+console.log(random);
 
-// console.log(ninjas.length);
+console.log(ninjas.length);
 
 
 
