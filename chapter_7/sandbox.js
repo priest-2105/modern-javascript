659b15079c92bf60f1e3665c1df36e5f366c8b42
priest-2105 <fawzybailey782@gmail.com>
2024-08-14 10:15:20 +0100

    commit

---
+++ chapter_7/sandbox.js
@@ -0,0 +1,24 @@
+let age = 25;
+
+
+// Loose Comparison ( Different type can still be equal )
+
+// console.log(age == 25 );
+// console.log(age == '25');
+// console.log(age != '25');
+// console.log(age != 25 );
+
+
+
+
+// Strict Comparison ( Different type cannot be equal )
+
+// console.log(age === 25 );
+// console.log(age === '25' );
+// console.log(age !== 25  );
+// console.log(age !== '25' );
+
+
+
+
+
---
+++ chapter_8/sandbox.js
@@ -0,0 +1,14 @@
+// Type conversion 
+
+let score = '100';
+
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
+
+
+// let result = Number('hello')
+// let result = String(50)
+let result = Boolean(0)
+
+console.log(result, typeof result);
