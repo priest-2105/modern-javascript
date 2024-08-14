f1efa3ab9a9964797aa59b5cc21f5a485539d2f5
priest-2105 <fawzybailey782@gmail.com>
2024-08-14 10:19:34 +0100

    commit

--- chapter_8/sandbox.js
+++ chapter_8/sandbox.js
@@ -10,6 +10,6 @@ let score = '100';
 // let result = Number('hello')
 // let result = String(50)
 // let result = Boolean(0)
-let result = Boolean('0')
+let result = Boolean('')
 
 console.log(result, typeof result);
