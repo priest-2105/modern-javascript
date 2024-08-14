df55c204002e02b3d85adeaee0668570455c3fbb
priest-2105 <fawzybailey782@gmail.com>
2024-08-14 10:30:11 +0100

    update ✨✨

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -3,20 +3,20 @@ let age = 25;
 
 // Loose Comparison ( Different type can still be equal )
 
-// console.log(age == 25 );
-// console.log(age == '25');
-// console.log(age != '25');
-// console.log(age != 25 );
+console.log(age == 25 );
+console.log(age == '25');
+console.log(age != '25');
+console.log(age != 25 );
 
 
 
 
 // Strict Comparison ( Different type cannot be equal )
 
-// console.log(age === 25 );
-// console.log(age === '25' );
-// console.log(age !== 25  );
-// console.log(age !== '25' );
+console.log(age === 25 );
+console.log(age === '25' );
+console.log(age !== 25  );
+console.log(age !== '25' );
 
 
 
