acd5fbe883aa92a9931df30fa51f800752c5f343
priest-2105 <fawzybailey782@gmail.com>
2024-08-14 10:28:56 +0100

    update ✨✨

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,14 +1,14 @@
-// let radius = 10
-// const pi = 3.14
+let radius = 10
+const pi = 3.14
 
 
-// console.log(radius, pi);
+console.log(radius, pi);
 
 
 // matth operators +, -, *,, / , **, %
 
 
-// console.log(10/2);
+console.log(10/2);
 
 // let result = radius % 3;
 // let result = pi * radius**2
@@ -19,24 +19,24 @@
 
 // let result = 5 * (10 - 3)**2;
 
-// console.log(result);
+console.log(result);
 
 
 
 // let likes = 10;
 
-// result = likes + 10;
+result = likes + 10;
 
-// likes++;
-// likes--;
+likes++;
+likes--;
 
-// result =likes += 10
-// like  -= 5;
-// likes *= 10;
-// likes /= 10;
+result =likes += 10
+like  -= 5;
+likes *= 10;
+likes /= 10;
 
 
-// console.log(likes);
+console.log(likes);
 
 
 
@@ -50,13 +50,13 @@
 
 // NAN - not a number
 
-// console.log(5 / 'Helo');
-// console.log(5 * 'Helo');
+console.log(5 / 'Helo');
+console.log(5 * 'Helo');
 
 
 // let result = 'the blog has ' + likes + 'likes';
 
-// console.log(result);
+console.log(result);
 
 
 
@@ -76,15 +76,15 @@ const likes = 30;
 
 // concatenation Way 
 
-// let result = 'The Blog called ' + title + 'by ' + author + 'has ' + likes + 'likes';
+let result = 'The Blog called ' + title + 'by ' + author + 'has ' + likes + 'likes';
 
-// console.log(result);
+console.log(result);
 
 
 // template string way 
 
 // let result = `The Blog called ${title} by ${author} has ${likes} likes  `
-// console.log(result);
+console.log(result);
 
 
 // creating html templates 
