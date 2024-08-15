dcde67c241a779ae17fe6efbc74114aee953aa72
priest-2105 <fawzybailey782@gmail.com>
2024-08-15 14:58:34 +0100

    commti ✨✨

--- chapter_14/sandbox.js
+++ chapter_14/sandbox.js
@@ -1,13 +1,13 @@
 //  Logicall Operatiors - OR || and AND &&
 
 
-const password = 'p@ss';
+const password = 'p@ss12';
 
 
 if (password.length >= 12 && password.includes('@')){
     console.log('that password is mighty strong');    
-} else if (password.length >= 8 || password.includes('@')){
-    console.log('that password is long enough!');
+} else if (password.length >= 8 || password.includes('@') && password.length >= 5){
+    console.log('that password is strong enough!');
 } else{
-    console.log('password is not long enough');
+    console.log('password is not strong enough');
 }
