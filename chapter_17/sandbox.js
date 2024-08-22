7242e5ef7426020a204f84afc2206248693d4bae
priest-2105 <fawzybailey782@gmail.com>
2024-08-23 00:20:16 +0100

    switch statements

---
+++ chapter_17/sandbox.js
@@ -0,0 +1,42 @@
+// Switch Statements
+
+const grade = '';
+
+
+switch(grade){
+
+    case 'A':
+        console.log('You Got an A !');
+        break;
+    case 'B':
+        console.log('You Got an B !');
+        break;
+    case 'C':
+        console.log('You Got a C !');
+        break;
+    case 'D':
+        console.log('You Got a D !');
+        break;
+    case 'E':
+        console.log('You Got an E !');
+        break;
+    default:
+        console.log('Not a valid grade');
+            
+}
+
+//  usoing if statements
+
+// if(grade === 'A'){
+
+// }else if ( grade === 'B'){
+    
+// }else if ( grade === 'D'){
+    
+// }else if ( grade === 'D'){
+    
+// }else if ( grade === 'E'){
+    
+// }else{
+
+// }
