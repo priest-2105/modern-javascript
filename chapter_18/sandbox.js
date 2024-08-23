3d7468b087a4a841d317d23f65e46504621d2bbb
priest-2105 <fawzybailey782@gmail.com>
2024-08-23 01:11:14 +0100

    code blocks

--- chapter_18/sandbox.js
+++ chapter_18/sandbox.js
@@ -1,6 +1,21 @@
 // Variable and Block Scope 
 
+let age = 30;
+
 
 if(true){
-    
+
+  let age = 40;  
+  let name = 'shawn'
+    console.log('inside 1ts code block', age, name);
+ 
+    if (true){
+        console.log('inside 2nd code block', age);
+        
+    }
+
 }
+
+
+console.log('outside code block', age, name);
+ 
