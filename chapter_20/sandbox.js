8aa2a4ac056138edda1367514782078ff1894bbc
priest-2105 <fawzybailey782@gmail.com>
2024-08-24 21:55:37 +0100

    Returning Values ✨✨

--- chapter_20/sandbox.js
+++ chapter_20/sandbox.js
@@ -1,1 +1,10 @@
 //  Arguments And parameters
+
+const speak = function(name = 'luigi', time = 'night'){
+    console.log(`Good ${time} ${name}`);   
+}
+
+// speak('mario', 'Morning');
+// speak();
+speak('shaun');
+
