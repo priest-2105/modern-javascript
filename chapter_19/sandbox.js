0c31f1dbe233caced60af0b02faba68aae05e812
priest-2105 <fawzybailey782@gmail.com>
2024-08-24 21:23:09 +0100

    arguments and patameters

--- chapter_19/sandbox.js
+++ chapter_19/sandbox.js
@@ -1,10 +1,24 @@
 //  Functions Declaration
-
 function greet (){
     console.log('Hello there');
-    
 }
+
+// greet();
+// greet();
+// greet();
+
 
 
 
 // function Expression 
+
+const speak = function(){
+    console.log('Good day');
+    
+};
+
+speak();
+speak(); 
+speak(); 
+
+
