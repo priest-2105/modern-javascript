a47063e123dfaf41bc1001fec8ba143f8e52cff1
priest-2105 <fawzybailey782@gmail.com>
2024-08-26 21:01:28 +0100

    arrow functions

---
+++ chapter_22/sandbox.js
@@ -0,0 +1,19 @@
+//  Returning Values
+
+// const speak = function(name = 'luigi', time = 'night'){
+//     console.log(`Good ${time} ${name}`);   
+// }
+
+const calcArea = function (radius) {
+    return 3.14 * radius**2; 
+};
+
+const area = calcArea(5);
+console.log(area);
+
+
+// const calcVol = function(area){
+
+// };
+
+// calcVol(area);
