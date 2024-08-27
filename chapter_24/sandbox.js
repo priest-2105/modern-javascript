e522b07770c60313aea58728cae14fb33f4df92a
priest-2105 <fawzybailey782@gmail.com>
2024-08-27 13:16:54 +0100

    calllbacks and foreach

--- chapter_24/sandbox.js
+++ chapter_24/sandbox.js
@@ -1,17 +1,27 @@
 // CallBacks and Foreach 
 
-const myFunc = (callbackFunc) => {
-    // do something 
-    let value = 50;
-    callbackFunc(value);
-};
+// const myFunc = (callbackFunc) => {
+//     // do something 
+//     let value = 50;
+//     callbackFunc(value);
+// };
 
 
-myFunc(function(value){
-    // do something 
-    console.log(value);
+// myFunc(value =>{
+//     // do something 
+//     console.log(value);
     
-})
+// })
 
 
-// let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+
+const logPerson = (person,index) =>{
+    console.log(`${index} - hello ${person}`);
+    
+}
+
+people.forEach(logPerson)
---
+++ chapter_25/sandbox.js
@@ -0,0 +1,15 @@
+// Get a reference to the UL 
+
+const ul = document.querySelector('.people');
+
+const people = [ 'mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+let html = ``;
+
+people.forEach(function(person){
+    // create html template 
+    html += `<li style="color:purple;">${person}<li>`
+})
+
+
+ul.innerHTML = html;
