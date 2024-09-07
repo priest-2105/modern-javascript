dfca0514d009037e9a2a669d3524de3ff9652f86
priest-2105 <fawzybailey782@gmail.com>
2024-09-07 01:46:10 +0100

    primitive and reference types✨✨

--- chapter_31/sandbox.js
+++ chapter_31/sandbox.js
@@ -1,1 +1,30 @@
-//  Primitive and Reference Types
+//  Primitive Type
+
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo : ${scoreTwo}`);
+
+
+scoreOne = 100;
+
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo : ${scoreTwo}`);
+
+
+
+
+
+
+// Reference Types 
+
+
+const userOne = { name: 'ryu', age: 30};
+const userTwo = userOne;
+
+
+console.log(userOne, userTwo);
+
+
+userTwo.age =  80;
+
+console.log(userOne, userTwo);
