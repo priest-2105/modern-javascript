271b493397d79b464e8e7818ec760f7d7d37a567
Bailey <priest2105@gmail.com>
2024-09-12 03:53:56 +0100

    style ✨✨

---
+++ chapter_36/sandbox.js
@@ -0,0 +1,13 @@
+ const title = document.querySelector('h1');
+
+
+//  title.setAttribute('style', 'margin: 50px;')
+
+ console.log(title.style);
+ 
+ console.log(title.style.color);
+ console.log(title.style.margin);
+ 
+
+ title.style.color = 'orange';
+ title.style.margin = '50px';
