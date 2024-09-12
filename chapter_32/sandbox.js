d3863353ce4410b8fb28a600c55e25c0bdfafae9
Bailey <priest2105@gmail.com>
2024-09-12 03:15:51 +0100

    dom

--- chapter_32/sandbox.js
+++ chapter_32/sandbox.js
@@ -1,2 +1,20 @@
 // DOM 
 
+// const paraEl = document.querySelector('p');
+
+// console.log(paraEl);
+
+
+// const parasEl = document.querySelectorAll('p');
+
+// const errorEl = document.querySelectorAll('.error');
+
+
+// parasEl.forEach(para => {
+//         console.log(para); 
+// })
+
+// console.log(parasEl[0]);
+
+// console.log(errorEl);
+
---
+++ chapter_33/sandbox.js
@@ -0,0 +1,13 @@
+// Geting an Element By ID 
+
+
+const titleEl = document.getElementById('page-title');
+
+console.log(titleEl);
+
+
+// Getting element by class name 
+
+const erros = document.getElementsByClassName('error');
+
+console.log(erros );
